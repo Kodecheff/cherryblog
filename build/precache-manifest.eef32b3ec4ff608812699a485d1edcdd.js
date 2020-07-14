365a2ea9541816ae3366ea1ca599856dd0ab5a78
kodecheff <akunneemeka125@gmail.com>
2020-07-14 18:10:32 +0100

    ran build script

---
+++ build/precache-manifest.eef32b3ec4ff608812699a485d1edcdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fca9488423a62daa9fd0a8037371a6",
+    "url": "/cherryblog/index.html"
+  },
+  {
+    "revision": "9d09664346d72c3460e7",
+    "url": "/cherryblog/static/css/main.67b0dce0.chunk.css"
+  },
+  {
+    "revision": "959bfa7cb5f24051620d",
+    "url": "/cherryblog/static/js/2.4b5dac70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cherryblog/static/js/2.4b5dac70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d09664346d72c3460e7",
+    "url": "/cherryblog/static/js/main.d9c8b11b.chunk.js"
+  },
+  {
+    "revision": "5c870dd4ffc266c35b45",
+    "url": "/cherryblog/static/js/runtime-main.a02973e5.js"
+  }
+]);
