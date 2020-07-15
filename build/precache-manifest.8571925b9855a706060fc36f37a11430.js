aae76ac388d6092774cdd634c74af7d16efdd83a
kodecheff <akunneemeka125@gmail.com>
2020-07-15 22:06:22 +0100

    better work

--- build/precache-manifest.8571925b9855a706060fc36f37a11430.js
+++ build/precache-manifest.8571925b9855a706060fc36f37a11430.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95fca9488423a62daa9fd0a8037371a6",
+    "revision": "bcc49ac84fbc82f6be7a23567d56abc0",
     "url": "/cherryblog/index.html"
   },
   {
-    "revision": "9d09664346d72c3460e7",
+    "revision": "f38ba7fbb344b073c35d",
     "url": "/cherryblog/static/css/main.67b0dce0.chunk.css"
   },
   {
-    "revision": "959bfa7cb5f24051620d",
-    "url": "/cherryblog/static/js/2.4b5dac70.chunk.js"
+    "revision": "730a7e7841c1c471bfc7",
+    "url": "/cherryblog/static/js/2.6ed62c15.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/cherryblog/static/js/2.4b5dac70.chunk.js.LICENSE.txt"
+    "url": "/cherryblog/static/js/2.6ed62c15.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d09664346d72c3460e7",
-    "url": "/cherryblog/static/js/main.d9c8b11b.chunk.js"
+    "revision": "f38ba7fbb344b073c35d",
+    "url": "/cherryblog/static/js/main.11012ce6.chunk.js"
   },
   {
     "revision": "5c870dd4ffc266c35b45",
